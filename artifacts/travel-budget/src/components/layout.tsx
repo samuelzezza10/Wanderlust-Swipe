6dd9b594c78bad996c751778557b56798a1ca251
samuelzezza10 <samuelzezza2025@libero.it>
2026-05-20 00:26:50 +0200

    UI rules: nav per tutti gli utenti, CTA Prenota, fallback trips, header area utente

--- artifacts/travel-budget/src/components/layout.tsx
+++ artifacts/travel-budget/src/components/layout.tsx
@@ -1,4 +1,3 @@
-import { Show } from "@clerk/react";
 import { DesktopNav } from "./desktop-nav";
 import { MobileNav } from "./mobile-nav";
 import { CookieBanner } from "./cookie-banner";
@@ -6,18 +5,14 @@ import { CookieBanner } from "./cookie-banner";
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-[100dvh] flex flex-col bg-background relative pb-16 md:pb-0">
-      <Show when="signed-in">
-        <DesktopNav />
-      </Show>
-      
+      <DesktopNav />
+
       <main className="flex-1 flex flex-col w-full max-w-7xl mx-auto md:px-8">
         {children}
       </main>
-      
-      <Show when="signed-in">
-        <MobileNav />
-      </Show>
-      
+
+      <MobileNav />
+
       <CookieBanner />
     </div>
   );
